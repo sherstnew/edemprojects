d9b4490bda44550b4313200ff85924a3a4ff33e8
sherstnew <sherstnev.denis.v@gmail.com>
2023-08-01 17:44:20 +0300

    adaptive fixes

--- src/components/Creator/Creator.tsx
+++ src/components/Creator/Creator.tsx
@@ -15,7 +15,7 @@ export function Creator (props: ICreatorProps) {
   return (
     <Link to={`/player?nickname=${player.nickname}`} className={styles.creator} target={blank ? '_blank' : '_self'}>
       <img src={`https://mineskin.eu/helm/${player.nickname}`} alt="Аватарка" className={styles.creator__avatar} />
-      <div className={player.isInRegistry ? styles.creator__nickname_red : styles.creator__nickname}>{player.nickname}</div>
+      <div className={player.isInRegistry ? styles.creator__nickname + ' ' + styles.creator__nickname_red : styles.creator__nickname}>{player.nickname}</div>
     </Link>
   );
 }
